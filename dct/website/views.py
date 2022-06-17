f94a77e6ec1f75188c2f4fc41bf67b397f97448f
NderituMwanu <mwanugerald@gmail.com>
2022-06-17 20:18:56 +0300

    application view functions

--- dct/website/views.py
+++ dct/website/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>page is working just fine</h1>')
+
+def sermons(request):
+    return render(request, 'website/index.html')
