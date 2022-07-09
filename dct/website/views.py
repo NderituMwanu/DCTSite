75956244d240553342f500d9bc829fec88f63509
NderituMwanu <mwanugerald@gmail.com>
2022-07-10 00:15:05 +0300

    new application views

--- dct/website/views.py
+++ dct/website/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
+from django.utils import timezone
 
 # Create your views here.
 def home(request):
     return HttpResponse('<h1>page is working just fine</h1>')
 
+def departments(request):
+    return render()
+
+def HomeMinistries(request):
+    return render()
+
+def about(request):
+    return render()
+
 def sermons(request):
-    return render(request, 'website/index.html')
+    date_today = datetime.datetime.now().date()
+    return render(request, 'website/index.html', {"date_today" : date_today})
